de41265faa1ddda96d3840139903e72c5475eb99
Fernando <fmorenilla23@gmail.com>
2026-03-11 17:09:44 +0100

    fix: CardContainer class

--- frontend/components/CardContainer.tsx
+++ frontend/components/CardContainer.tsx
@@ -1,7 +1,7 @@
 
 export default function CardContainer({ children, className }: { children: React.ReactNode; className?: string }) {
 	return (
-		<div className={`flex-1 bg-card p-8 rounded-lg shadow-sm border-2 border-border ${className}`}>
+		<div className={`bg-card p-8 rounded-lg shadow-sm border-2 border-border ${className}`}>
 			{children}
 		</div>
 	)
