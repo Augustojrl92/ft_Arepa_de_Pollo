e9959b8b8dca39657135b98471edba7e7fdb717e
Fernando <fmorenilla23@gmail.com>
2026-03-11 21:50:18 +0100

    fix: Clean Mockup selector

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,7 +4,6 @@ import UserCard from "./_components/UserCard"
 import TournamentCard from "./_components/TournamentCard"
 import CoalitionCard from "./_components/CoalitionCard"
 import { User } from "@/types"
-import { useState } from "react"
 
 const mockUser: User = {
 	avatar: 'https://i.pravatar.cc/150?img=51',
@@ -14,7 +13,7 @@ const mockUser: User = {
 	coalitionPoints: 89000,
 	coalitionRank: 42,
 	walletAmount: 1337,
-	evalPoints: 6
+	evalPoints: 6,
 }
 
 const mockCoalitions = [
@@ -25,33 +24,12 @@ const mockCoalitions = [
 ]
 
 export default function Home() {
-	const [user, setUser] = useState<User>(mockUser)
-	const [modalUser, setModalUser] = useState<Boolean>(false)
-
-	const handleChangeUser = () => {
-		const newCoalition = (document.getElementById('coalition') as HTMLInputElement).value
-		setUser(prev => ({ ...prev, coalition: newCoalition }))
-	}
 
 	return (
 		<section className="py-5 flex gap-5">
-			<UserCard user={user} />
+			<UserCard user={mockUser} />
 			<TournamentCard coalitions={mockCoalitions} />
-			<CoalitionCard user={user} />
-			<div className="absolute top-4 left-8">
-				<button className="cursor-pointer bg-card-hover px-4 py-2 rounded-lg" onClick={() => setModalUser(prev => !prev)}>Mock User</button>
-				{modalUser && (
-					<div className="p-10 bg-card/80 rounded-lg shadow-lg">
-						<label htmlFor="coalition">Coalition</label>
-						<select id="coalition" defaultValue={user.coalition} className="border p-2 rounded mb-2 w-full" onChange={handleChangeUser}>
-							<option value="zefiria">Zefiria</option>
-							<option value="tiamant">Tiamant</option>
-							<option value="marventis">Marventis</option>
-							<option value="ignisaria">Ignisaria</option>
-						</select>
-					</div>
-				)}
-			</div>
+			<CoalitionCard user={mockUser} />
 		</section>
 	)
 }
