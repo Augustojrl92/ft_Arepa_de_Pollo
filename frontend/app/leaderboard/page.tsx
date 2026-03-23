76d6b4750eed41a8f768cc91fd0088b77778536f
Fernando <fmorenilla23@gmail.com>
2026-03-23 20:49:09 +0100

    feat: Leaderboard first ahead and coalition individual page

--- frontend/app/leaderboard/page.tsx
+++ frontend/app/leaderboard/page.tsx
@@ -1,7 +1,27 @@
-export default function Prueba() {
+'use client'
+
+import { useEffect } from "react"
+import { useCoalitionStore } from "@/hooks"
+import CoalitionCard from "./_components/CoalitionCard"
+
+export default function Leaderboard() {
+	const { coalitions, setCoalitions, maxPoints } = useCoalitionStore()
+
+	useEffect(() => {
+		setCoalitions()
+	}, [setCoalitions])
+
+	const orderedCoalitions = [...coalitions].sort((a, b) => b.points - a.points)
+
 	return (
-		<section className="p-4">
-			<h1 className="text-2xl font-bold">LeaderBoard</h1>
+		<section className="py-5">
+			<ul className="flex flex-col gap-5">
+				{orderedCoalitions.map((coalition, index) => (
+					<li key={coalition.id}>
+						<CoalitionCard coalition={coalition} index={index} isLeader={coalition.points === maxPoints} maxPoints={maxPoints} />
+					</li>
+				))}
+			</ul>
 		</section>
 	)
 }
