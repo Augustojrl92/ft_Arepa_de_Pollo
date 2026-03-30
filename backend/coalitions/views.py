fd90e22184eecdbf1a5cc31c8669037abe32eed2
Fernando <fmorenilla23@gmail.com>
2026-03-30 15:33:38 +0200

    refactor: Backend refactor for use internal data instead of 42 API responses

--- backend/coalitions/views.py
+++ backend/coalitions/views.py
@@ -3,7 +3,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .services import _serialize_coalition_leaderboard
+from .services import _serialize_coalition_leaderboard, _serialize_simple_coalitions
 
 
 class CoalitionLeaderboardView(APIView):
@@ -12,5 +12,25 @@ class CoalitionLeaderboardView(APIView):
 	def get(self, request):
 		return Response(
 			{'coalitions': _serialize_coalition_leaderboard()},
+			status=status.HTTP_200_OK,
+		)
+
+class CoalitionSimpleView(APIView):
+	permission_classes = [IsAuthenticated]
+
+	def get(self, request):
+		coalition_slug = request.query_params.get('slug')
+
+		if coalition_slug:
+			coalition = _serialize_simple_coalitions(coalition_slug=coalition_slug)
+			if coalition is None:
+				return Response(
+					{'error': 'Coalition not found'},
+					status=status.HTTP_404_NOT_FOUND,
+				)
+			return Response({'coalition': coalition}, status=status.HTTP_200_OK)
+
+		return Response(
+			{'coalitions': _serialize_simple_coalitions()},
 			status=status.HTTP_200_OK,
 		)
