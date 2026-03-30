fbbcb5262fe7285086c2e2f047479b9118f375c5
Augusto Jesus Rodriguez Linares <aurodrig@c2r18s1.42madrid.com>
2026-03-30 19:17:34 +0200

    feat: add user ranking endpoint

--- backend/coalitions/urls.py
+++ backend/coalitions/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import CoalitionLeaderboardView, CoalitionSimpleView
+from .views import CoalitionLeaderboardView, CoalitionSimpleView, UserRankingView
 
 
 urlpatterns = [
 	path('', CoalitionSimpleView.as_view(), name='coalition-simple'),
 	path('leaderboard/', CoalitionLeaderboardView.as_view(), name='coalition-leaderboard'),
+	path('users-ranking/', UserRankingView.as_view(), name='user-ranking'),
 ]
