cc805451813e6f5d61f56f78bdc1549333893c19
Fernando <fmorenilla23@gmail.com>
2026-04-01 00:32:31 +0200

    feat: Coalitions page and components updates

--- frontend/components/CardContainer.tsx
+++ frontend/components/CardContainer.tsx
@@ -1,7 +1,7 @@
 
-export default function CardContainer({ children, className }: { children: React.ReactNode; className?: string }) {
+export default function CardContainer({ children, className, style }: { children: React.ReactNode; className?: string; style?: React.CSSProperties }) {
 	return (
-		<div className={`bg-card p-8 rounded-lg shadow-sm border-2 border-border ${className}`}>
+		<div className={`bg-card p-8 rounded-lg shadow-sm border-2 border-border ${className}`} style={style}>
 			{children}
 		</div>
 	)
