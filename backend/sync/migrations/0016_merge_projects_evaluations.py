2630b9670ad03e977486583da146b963fbc74620
Augusto Jesus Rodriguez Linares <aurodrig@c3r7s1.42madrid.com>
2026-04-17 10:13:12 +0200

    fix: merge sync project and evaluation migrations

---
+++ backend/sync/migrations/0016_merge_projects_evaluations.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+	dependencies = [
+		('sync', '0014_coalitionprojectcursor'),
+		('sync', '0015_campususer_coalitions_user_id_and_cursor'),
+	]
+
+	operations = []
