523bd47f7665f57a639d7282bef51f406d47b59a
GGasset <gassetgerman@gmail.com>
2026-07-14 16:50:47 +0200

    added active parameter to _serialize_user_details

--- backend/users/services.py
+++ backend/users/services.py
@@ -1,3 +1,5 @@
+from time import time
+
 from django.contrib.auth import get_user_model
 from django.db import transaction
 
@@ -5,6 +7,7 @@ from django.contrib.auth.models import User
 from sync.models import CampusUser
 from .models import FriendsList
 
+time_until_inactivity = 2 * 60
 
 class FriendsRequestError(Exception):
 	def __init__(self, message, http_status):
@@ -42,6 +45,9 @@ def _serialize_user_details(user_login, request=None):
 	has_account = owner is not None
 	avatar_url = _resolve_avatar_url(owner, campus_user.avatar_url, request=request)
 
+	active = time() - campus_user.last_active_time < time_until_inactivity
+	print(active)
+
 	return {
 		'id': campus_user.user_id,
 		'login': campus_user.login,
@@ -54,8 +60,8 @@ def _serialize_user_details(user_login, request=None):
 		'coalition_rank': campus_user.coalition_rank,
 		'has_account': has_account,
 		'general_rank': campus_user.general_rank,
-		'achievements': 'none'  # Placeholder for achievements data,
-
+		'achievements': 'none',  # Placeholder for achievements data,
+		'active': active
 	}
 
 def _serialize_friend_entry(friend_list, request=None):
