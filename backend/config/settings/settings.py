985da383438b8d29a206bd25942fdb99bb9e5def
GGasset <gassetgerman@gmail.com>
2026-07-14 16:52:06 +0200

    added middleware to set the time of the last request the user made

--- backend/config/settings/settings.py
+++ backend/config/settings/settings.py
@@ -70,6 +70,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
+	'users.middleware.OnlineStatusMiddleware'
 ]
 
 ROOT_URLCONF = 'config.urls'
