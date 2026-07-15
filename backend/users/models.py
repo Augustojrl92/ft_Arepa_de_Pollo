06a392be494177dc903115b26863d9cb2fa95ef7
GGasset <gassetgerman@gmail.com>
2026-07-15 16:32:23 +0200

    made achievement's name unique

--- backend/users/models.py
+++ backend/users/models.py
@@ -40,7 +40,7 @@ class Achievement(models.Model):
 
 	# HTML for the achievement icon to be inserted into the page. The default value is a copy of the html of the div of a mock up icon
 	icon_HTML = models.TextField(default='<div class="inline-flex rounded-lg border border-border bg-card p-2"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-medal h-5 w-5 text-(--coalition-color)"><path d="M7.21 15 2.66 7.14a2 2 0 0 1 .13-2.2L4.4 2.8A2 2 0 0 1 6 2h12a2 2 0 0 1 1.6.8l1.6 2.14a2 2 0 0 1 .14 2.2L16.79 15"></path><path d="M11 12 5.12 2.2"></path><path d="m13 12 5.88-9.8"></path><path d="M8 7h8"></path><circle cx="12" cy="17" r="5"></circle><path d="M12 18v-2h-.5"></path></svg></div>')
-	name = models.TextField(default='Dios de las arepas')
+	name = models.TextField(default='Dios de las arepas', unique=True)
 	description = models.TextField(default='Has diseñado la Arepacendence definitiva')
 
 	# Points needed for completion of the achivement, it may be a percentage (set to 100), a bool (set to 1), or any arbitrary number
