0717a242a029e298f5590deaf9ca8e3c5f860a00
GGasset <gassetgerman@gmail.com>
2026-07-22 16:05:07 +0200

    modified modifier

--- backend/users/achievement_functions.py
+++ backend/users/achievement_functions.py
@@ -16,7 +16,7 @@ def set_up_achievements():
 
 	coalition_rank_name = 'Vengo a ver a mis fieles seguidores.'
 	Achievement.completion_check_funcs[coalition_rank_name] = coalition_rank_completion
-	create_achievement_if_unexistant(coalition_rank_name, 'Inicia sesión en la plataforma siendo el primero de tu coalición', 1)
+	create_achievement_if_unexistant(coalition_rank_name, 'Sé el primero de tu coalición', 1)
 
 
 def create_achievement_if_unexistant(name: str, description: str, completion_points: int, icon_HTML: str | None = None) -> None:
